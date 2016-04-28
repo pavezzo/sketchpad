6c95f24ebca912113e7ef4bdfffca9c30ea0603d
Paavo Nätti <paavo.natti@gmail.com>
2016-04-28 22:07:42 +0300

    update

--- skript.js
+++ skript.js
@@ -2,10 +2,19 @@ var width = Math.floor($(window).width());
 var height = Math.floor($(window).height());
 var area = (width * height) / 256;
 
+// var koko = function() {
+//     var korkeus = prompt("How high the grid should be?");
+//     var leveys = prompt("How wide the grid should be?");
+//     korkeus();
+//     leveys();
+//     if (korkeus < height && leveys < width) {  
+
+//     }
+// } 
 
 
 $(document).ready(function() {
-    for (i = 1; i < (area); i++) {
+    for (i = 1; i < 1200; i++) {
         $('#container').append('<div class="block"></div>');
     }
     $('.block').mouseenter(function() {
