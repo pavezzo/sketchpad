54c06d0389ac1c4ccf8dfaaf5e97912f3dd8180f
Paavo Nätti <paavo.natti@gmail.com>
2016-04-30 00:59:13 +0300

    soon ready

--- skript.js
+++ skript.js
@@ -1,26 +1,30 @@
-var width = Math.floor($(window).width());
-var height = Math.floor($(window).height());
-var area = (width * height) / 256;
+var korkeus = prompt("How high the grid should be?");
+var leveys = prompt("How wide the grid should be?");
+var korkeus16 = (korkeus * 16);
+var leveys16 = (leveys * 16);
+leveys;
+var yakseli = ($(window).height() / (korkeus * 16));
+var xakseli = ($(window).width() / (leveys * 16));
 
-// var koko = function() {
-//     var korkeus = prompt("How high the grid should be?");
-//     var leveys = prompt("How wide the grid should be?");
-//     korkeus();
-//     leveys();
-//     if (korkeus < height && leveys < width) {  
 
-//     }
-// } 
+
+var koko = Math.ceil(korkeus * leveys) + 1;
 
 
 $(document).ready(function() {
-    for (i = 1; i < 1200; i++) {
+    $('#container').css("height", korkeus16);
+    $('#container').css("width", leveys16);
+    $('#container').css("left", xakseli);
+    $('#container').css("right", xakseli);
+    $('#container').css("top", yakseli);
+    $('#container').css("bottom", yakseli);
+    for (i = 1; i < koko; i++) {
         $('#container').append('<div class="block"></div>');
     }
     $('.block').mouseenter(function() {
         $(this).css("background-color", "blue");
     })
-    $('button').click(function() {
+    $('#clear').click(function() {
         $('.block').css("background-color", "initial");    
     })
 });
